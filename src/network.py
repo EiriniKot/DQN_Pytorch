783b5480e9d859707e03a61adb6793391cda797e
EiriniKot <eirkotz@gmail.com>
2023-02-21 15:12:37 +0200

    remove print

--- src/network.py
+++ src/network.py
@@ -28,7 +28,6 @@ class DqnNet(nn.Module):
         self.head = nn.Linear(linear_input_size, outputs)
 
     def forward(self, x):
-        print(x.shape)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
