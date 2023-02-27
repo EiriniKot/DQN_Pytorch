54d6aef46f32d8d15b44617842c8925ee30c499e
EiriniKot <eirkotz@gmail.com>
2023-02-27 22:53:42 +0200

    edit device siameze

--- train_inverse.py
+++ train_inverse.py
@@ -11,17 +11,22 @@ json_config = json.load(f)
 buffer = ReplayMemory(capacity=None, **json_config['sampling'])
 dt_iter = ExperienceDataset(buffer)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 encoder = Encoder(h=json_config['h_frame'],
                   w=json_config['w_frame'],
-                  enc_size=json_config['enc_size'])
+                  enc_size=json_config['enc_size']).to(device)
 
-inverse = Inverse(emb_size=json_config['enc_size'], output_size=18)
+inverse = Inverse(emb_size=json_config['enc_size'], output_size=18).to(device)
 trainer = SiamezeTrainer(encoder, inverse, tensorboard=False)
 
 models_save = os.path.join(os.getcwd(), 'models')
 
+buffer = ReplayMemory(capacity=json_config['replay_capacity'], device = device, **json_config['sampling'])
+dt_iter = ExperienceDataset(buffer, path_folder='/saved_games')
+
 for epoch_indx in range(1):
-   last_loss = trainer.train_one_epoch(epoch_indx, dt_iter)
+  last_loss = trainer.train_one_epoch(epoch_indx, dt_iter, training_batch=1)
 
 torch.save({'epoch': epoch_indx,
             'model_state_dict': encoder.state_dict(),
