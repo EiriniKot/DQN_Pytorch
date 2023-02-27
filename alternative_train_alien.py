de23361a3b0691525ff29ed0850263f7c8638e60
EiriniKot <eirkotz@gmail.com>
2023-02-28 00:16:44 +0200

    fix copy

--- alternative_train_alien.py
+++ alternative_train_alien.py
@@ -8,7 +8,7 @@ from src.nn_utils import GamesRunner
 if __name__ == '__main__':
     f = open('envs.json')
     json_config = json.load(f)
-    dqn_net = DqnNetAlternative(h=json_config['h_frame'],
+    policy_net = DqnNetAlternative(h=json_config['h_frame'],
                                 w=json_config['w_frame'],
                                 enc_size=json_config['enc_size'],
                                 emb_depth=json_config['emb_depth'],
@@ -16,6 +16,17 @@ if __name__ == '__main__':
                                 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
                                 encoder_path='models/encoder.pt',
                                 embed_path='models/embedding.pt')
+
+    target_net = DqnNetAlternative(h=json_config['h_frame'],
+                                   w=json_config['w_frame'],
+                                   enc_size=json_config['enc_size'],
+                                   emb_depth=json_config['emb_depth'],
+                                   n_actions=json_config['n_actions'],
+                                   device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+                                   encoder_path='models/encoder.pt',
+                                   embed_path='models/embedding.pt')
+
+    target_net.load_state_dict(policy_net.state_dict())
 
     runner = GamesRunner(json_config,
                          batch=json_config['batch_size'],
@@ -26,8 +37,8 @@ if __name__ == '__main__':
                          max_iterations_ep=json_config['max_iterations_ep'],
                          capacity=json_config['replay_capacity'],
                          device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-                         p_net=dqn_net,
-                         t_net=dqn_net,
+                         p_net=policy_net,
+                         t_net=target_net,
                          save_buffer=False,
                          num_episodes=60)
 
