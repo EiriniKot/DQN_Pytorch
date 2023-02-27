58ae8983308ede86282c817dc6627ceecebbbfb7
EiriniKot <eirkotz@gmail.com>
2023-02-27 22:44:23 +0200

    update gitignore

--- train_inverse.py
+++ train_inverse.py
@@ -18,7 +18,7 @@ encoder = Encoder(h=json_config['h_frame'],
 inverse = Inverse(emb_size=json_config['enc_size'], output_size=18)
 trainer = SiamezeTrainer(encoder, inverse, tensorboard=False)
 
-models_save = '/home/eirini/PycharmProjects/DQN_Pytorch/models_inverse_encoded'
+models_save = os.path.join(os.getcwd(), 'models')
 
 for epoch_indx in range(1):
    last_loss = trainer.train_one_epoch(epoch_indx, dt_iter)
